87180d229e5fd9c16fe3c149793f81ac60ba4d1b
hataboon <straightener0201@hotmail.co.jp>
2024-10-11 13:45:24 +0900

    フラッシュメッセージ追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [ :create ]
 
   # GET /resource/sign_in
   # def new
@@ -11,15 +11,15 @@ class Users::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     user = User.find_by(email: params[:email])
-    
+
     # パスワードの認証を確認
     if user&.valid_password?(params[:password])
       # ログイン成功
       sign_in(user)
-      redirect_to root_path, notice: 'ログインに成功しました。'
+      redirect_to root_path, notice: "ログインに成功しました。"
     else
       # ログイン失敗
-      flash.now[:alert] = 'メールアドレスまたはパスワードが間違っています。'
+      flash.now[:alert] = "メールアドレスまたはパスワードが間違っています。"
       render :new, status: :unprocessable_entity
     end
   end
@@ -33,6 +33,6 @@ class Users::SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :password, :remember_me ])
   end
 end
