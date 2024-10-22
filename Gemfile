439172588875bd91189d2efa4d2d60c5e7aa5179
hataboon <straightener0201@hotmail.co.jp>
2024-10-22 22:22:52 +0900

    openAI APIの導入

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem "devise-i18n"
 gem "dotenv-rails"
 gem "httpclient"
 
+gem 'ruby-openai'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,51 @@
 class RecipesController < ApplicationController
-  require "httpclient"
-  def index
-    if params[:search].blank?
-      query = "banana"
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    if params[:ingredients].present?
+      # 食材をOpenAIで翻訳
+      translated_ingredients = params[:ingredients].map { |ingredient| translate_ingredient(ingredient) }
+      query = translated_ingredients.reject(&:blank?).join(",")
     else
-      query = params[:search]
+      @foods = []
+      render :new
+      return
     end
+
+    # Edamam APIリクエスト
     client = HTTPClient.new()
-    res = client.get("https://api.edamam.com/api/recipes/v2?type=public&q=#{query}&app_id=#{ENV['application_id']}&app_key=#{ENV['application_keys']}")
-    @foods = JSON.parse(res.body)
+    res = client.get("https://api.edamam.com/api/recipes/v2?type=public&q=#{CGI.escape(query)}&app_id=#{ENV['application_id']}&app_key=#{ENV['application_keys']}")
+    recipes = JSON.parse(res.body)
+
+    @recipes = recipes["hits"]
+    render :show
+  end
+
+  private
+
+  def generate_meal_plan(recipes, duration)
+    client = OpenAI::Client.new
+
+    response = client.chat(
+      parameters: {
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: "Create a meal plan for #{duration} days using the following recipes: #{recipes}" }],
+        max_tokens: 300
+      }
+    )
+    response['choices'].first['message']['content']
+  end
+
+  def translate_ingredient(ingredient)
+    client = OpenAI::Client.new
+    response = client.chat(
+      parameters: {
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: "Translate the following ingredient to English: #{ingredient}" }]
+      }
+    )
+    response['choices'].first['message']['content'].strip
   end
 end
