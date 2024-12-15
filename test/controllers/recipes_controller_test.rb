bcc60e58bcd2ecf447fe8a20cfdea63ab6721102
hataboon <straightener0201@hotmail.co.jp>
2024-12-15 22:39:25 +0900

    献立作成、カレンダー機能

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,13 +1,3 @@
-require "test_helper"
-
-class RecipesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
-
-require "test_helper"
-
 class RecipesControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get new_recipe_url
