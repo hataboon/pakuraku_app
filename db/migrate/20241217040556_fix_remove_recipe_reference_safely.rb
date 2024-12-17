5076fb8d9bb6e9989eb3ecf8aa8bf4828868dfdb
hataboon <straightener0201@hotmail.co.jp>
2024-12-17 13:14:44 +0900

    Fix migration and successfully remove recipe reference

--- db/migrate/20241217040556_fix_remove_recipe_reference_safely.rb
+++ db/migrate/20241217040556_fix_remove_recipe_reference_safely.rb
@@ -1,17 +1,13 @@
 class FixRemoveRecipeReferenceSafely < ActiveRecord::Migration[6.0]
   def change
-    reversible do |dir|
-      dir.up do
-        # 外部キーが存在する場合のみ削除
-        if foreign_key_exists?(:nutritions, :recipe)
-          remove_reference :nutritions, :recipe, foreign_key: true
-        end
-      end
-
-      dir.down do
-        # ロールバック時に追加
-        add_reference :nutritions, :recipe, foreign_key: true
-      end
+    # 外部キーが存在する場合のみ削除
+    if foreign_key_exists?(:nutritions, :recipe)
+      remove_reference :nutritions, :recipe, foreign_key: true
+    else
+      puts "Foreign key for 'recipes' already removed, skipping."
     end
+
+    # 新しいリファレンスを追加
+    add_reference :nutritions, :food, foreign_key: true
   end
 end
