cb6b3c9920a7738298f871a6456ac016a99a8e10
hataboon <straightener0201@hotmail.co.jp>
2024-12-29 23:11:09 +0900

    css設定の修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,8 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("app/assets/stylesheets")
-Rails.application.config.assets.precompile += %w( application.tailwind.css )
-
+Rails.application.config.assets.precompile += %w( 
+  application.tailwind.css
+  application.css
+)
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
