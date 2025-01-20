0617c84f222f5f136bf2530ce6f6a0e53ce9351d
hataboon <straightener0201@hotmail.co.jp>
2025-01-20 12:45:46 +0900

    Remove invalid asset_host from CSP config

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,19 +4,22 @@ Rails.application.config.content_security_policy do |policy|
   # 基本設定
   policy.default_src :self, :https
   policy.font_src    :self, :https, :data
-  policy.img_src     :self, :https, :data, :blob
+  policy.img_src     :self, :https, :data, :blob, '*'  # '*'を追加
   policy.script_src  :self, :https, "'unsafe-inline'"
   policy.style_src   :self, :https, "'unsafe-inline'"
-
+  
   # Twitter連携用
   policy.connect_src :self, :https, "https://twitter.com"
   policy.frame_src   :self, :https, "https://twitter.com"
-
+  
   # Render用（本番環境のみ）
   if Rails.env.production?
-    host = "pakuraku-app.onrender.com"
+    host = ENV['RENDER_EXTERNAL_URL'] || "pakuraku-app.onrender.com"
     policy.connect_src :self, :https, "https://#{host}", "wss://#{host}"
-    policy.img_src     :self, :https, :data, :blob, "https://#{host}"
-    policy.asset_host  "https://#{host}"
+    policy.img_src     :self, :https, :data, :blob, "https://#{host}", '*'
+    # 削除: policy.asset_host "https://#{host}"
   end
 end
+
+# 開発環境でのみレポートモードを有効化
+Rails.application.config.content_security_policy_report_only = Rails.env.development?
