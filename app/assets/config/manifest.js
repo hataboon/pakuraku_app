210b0b133b5f1a36bc47ac4a037f7a4e1e7c4128
hataboon <straightener0201@hotmail.co.jp>
2025-01-22 15:32:38 +0900

    エラーの解決

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
-// app/assets/config/manifest.js
 //= link_tree ../images
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+//= link_directory ../stylesheets .css
 //= link application.tailwind.css
+//= link application.js
