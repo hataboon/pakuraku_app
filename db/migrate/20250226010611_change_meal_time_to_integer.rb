e206a1069a5ec3821f63ee27ba7e86dd5a29fb45
hataboon <straightener0201@hotmail.co.jp>
2025-03-01 12:56:55 +0900

    Fix migration for meal_time conversion to integer

--- db/migrate/20250226010611_change_meal_time_to_integer.rb
+++ db/migrate/20250226010611_change_meal_time_to_integer.rb
@@ -1,9 +1,48 @@
 class ChangeMealTimeToInteger < ActiveRecord::Migration[8.0]
   def up
-    change_column :calendar_plans, :meal_time, 'integer USING CAST(meal_time AS integer)'
+    # まず一時的なカラムを作成
+    add_column :calendar_plans, :meal_time_integer, :integer
+
+    # 既存のデータを変換して一時カラムに保存
+    execute <<-SQL
+      UPDATE calendar_plans 
+      SET meal_time_integer = CASE 
+        WHEN meal_time = 'morning' THEN 0
+        WHEN meal_time = 'afternoon' THEN 1
+        WHEN meal_time = 'evening' THEN 2
+        WHEN meal_time = '0' THEN 0
+        WHEN meal_time = '1' THEN 1 
+        WHEN meal_time = '2' THEN 2
+        ELSE 0
+      END
+    SQL
+
+    # 元のカラムを削除
+    remove_column :calendar_plans, :meal_time
+
+    # 新しいカラムの名前を元の名前に変更
+    rename_column :calendar_plans, :meal_time_integer, :meal_time
   end
 
   def down
-    change_column :calendar_plans, :meal_time, :string
+    # 元に戻す場合の処理
+    add_column :calendar_plans, :meal_time_string, :string
+
+    # 整数から文字列に戻す
+    execute <<-SQL
+      UPDATE calendar_plans 
+      SET meal_time_string = CASE 
+        WHEN meal_time = 0 THEN 'morning'
+        WHEN meal_time = 1 THEN 'afternoon'
+        WHEN meal_time = 2 THEN 'evening'
+        ELSE 'morning'
+      END
+    SQL
+
+    remove_column :calendar_plans, :meal_time
+    rename_column :calendar_plans, :meal_time_string, :meal_time
   end
 end
+
+
+
